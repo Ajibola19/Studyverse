90227aad73ba09119c941862ff2c674a59095c02
Ajibola19 <162378245+Ajibola19@users.noreply.github.com>
2026-05-13 17:47:50 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,13 +1,16 @@
 <?php
-// connection.php
-$host = "sql101.infinityfree.com"; // Your MySQL Hostname from the panel
-$user = "if0_41894971";             // Your MySQL Username from the panel
-$pass = "Ajifoden19";     // Your account password
-$db   = "if0_41894971_study_verse";  // The full DB name created in the panel
+$host = 'gateway01.us-east-1.prod.aws.tidbcloud.com';
+$port = 4000;
+$user = '3yvdKWJpMp4vpEP.root'; 
+$pass = 'pi8kCfVEw0bU6bsA'; // The password you created in TiDB
+$db   = 'studyverse_db'; 
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = mysqli_init();
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+// This line is MANDATORY for TiDB Cloud to work on Render
+mysqli_ssl_set($conn, NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
+
+if (!mysqli_real_connect($conn, $host, $user, $pass, $db, $port, NULL, MYSQLI_CLIENT_SSL)) {
+    die("Connection failed: " . mysqli_connect_error());
 }
 ?>
